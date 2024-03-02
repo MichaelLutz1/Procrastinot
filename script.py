460ad3aecd777dbf60e0a65aafef5651fcc5ca11
Michael Lutz <mikelutz2400@gmail.com>
2024-03-02 13:21:23 -0500

    print screen text

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 from PIL import ImageGrab
+import pytesseract
 
 # Take a screenshot of the entire screen
 screenshot = ImageGrab.grab()
-
+print(pytesseract.image_to_string(screenshot))
 # Save the screenshot to a file
-screenshot.save("screenshot.png")
 
 # Display the screenshot
 screenshot.show()
