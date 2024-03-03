617e5ca9ae1f9f8f9d8344124a755366a66acd13
Michael Lutz <mikelutz2400@gmail.com>
2024-03-02 20:53:35 -0500

    Button functionality

--- script.py
+++ script.py
@@ -11,10 +11,11 @@ import Levenshtein
 import Levenshtein
 
 app = Flask(__name__)
-
+stop_flag = threading.Event()
+text_thread = None
 top_left = bottom_right = None
 textType = ""
-#pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 
 '''
@@ -28,6 +29,7 @@ backSurface = pygame.Surface((screenWidth, screenHeight))
 backSurface.fill((0, 0, 0))
 '''
 
+
 def on_click(x, y, button, pressed):
     global top_left, bottom_right
     if pressed and not top_left:
@@ -37,14 +39,17 @@ def on_click(x, y, button, pressed):
         bottom_right = (int(x), int(y))
         print('Bottom right:', bottom_right)
         return False
-    if not pressed:
-        return True
+
 
 previous_text = ""
 
+
 def screen_to_text(top_left, bottom_right):
-    screenshot = ImageGrab.grab(
-        bbox=(top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
+    if not top_left and not bottom_right:
+        screenshot = ImageGrab.grab()
+    else:
+        screenshot = ImageGrab.grab(
+            bbox=(top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
     text = pytesseract.image_to_string(screenshot)
     return text
 
@@ -58,23 +63,21 @@ def start():
 
     main()
 
-#If a chain of consecutive nodes has the same text, this is considered inactive time. Otherwise if a node has the same text as another node after having lots of variation in between, changes have been undone and the nodes between can be deleted.
+# If a chain of consecutive nodes has the same text, this is considered inactive time. Otherwise if a node has the same text as another node after having lots of variation in between, changes have been undone and the nodes between can be deleted.
+
+
 def checkChangeRevert(linkedList, currText):
     currNode = linkedList.getFirst()
-    while(currNode != None):
+    while (currNode != None):
         similarity = Levenshtein.ratio(currText, currNode.text)
         print(similarity)
         currNode = currNode.next
 
 
 def main():
-    with mouse.Listener(
-            on_click=on_click,
-    ) as listener:
-        listener.join()
 
     linkedList = LinkedList(screen_to_text(top_left, bottom_right))
-    while True:
+    while not stop_flag.is_set():
         currText = screen_to_text(top_left, bottom_right)
         checkChangeRevert(linkedList, currText)
         linkedList.insertFirst(currText)
@@ -85,8 +88,8 @@ def main():
     linkedList.insertFirst("ihi")
     linkedList.insertFirst("hiiiiiiiiiiiiiii")
     linkedList.printList()
-    #linkedList.pop()
-    #linkedList.printList()
+    # linkedList.pop()
+    # linkedList.printList()
     checkChangeRevert(linkedList, "hi")
 
     '''
@@ -98,31 +101,55 @@ def main():
 
         screen.blit(backSurface, (0, 0))
         font = pygame.font.Font(None, 32)
-        text = font.render(screen_to_text(top_left, bottom_right), True, "green")
+        text = font.render(screen_to_text(
+            top_left, bottom_right), True, "green")
         textRect = text.get_rect()
         screen.blit(text, textRect)
-         
+
         pygame.display.update()
         clock.tick(30)
-    '''
-#start() ####
+    '''  # start() ####
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if (request.method == 'POST'):
-        text_thread = threading.Thread(target=main)
-        text_thread.start()
-    return render_template('index.html', size=[top_left, bottom_right])
+    if text_thread and text_thread.is_alive():
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
+
 
 @app.route('/get_text', methods=['GET'])
 def get_text():
-    if (request.method == 'Get'):
-        text_thread = threading.Thread(target=main)
-        text_thread.start()
-    return screen_to_text(top_left, bottom_right)
+    if (request.method == 'GET'):
+        active = request.args.get('active')
+        global text_thread
+        if (active == "true"):
+            text_thread = threading.Thread(target=main)
+            text_thread.start()
+            return "started"
+        else:
+            if (text_thread and text_thread.is_alive()):
+                global stop_flag
+                stop_flag.set()
+                text_thread.join()
+            return "stopped"
+    return render_template('index.html')
+
+
+@app.route('/resize', methods=['GET'])
+def get_size():
+    print("Resizing")
+    global top_left, bottom_right
+    top_left = None
+    bottom_right = None
+    with mouse.Listener(
+            on_click=on_click,
+    ) as listener:
+        listener.join()
+    print("Top left:", top_left, "Bottom right:", bottom_right)
+    return 'resized'
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=2400, debug=True)
-
-
